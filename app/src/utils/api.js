92c7fa30584171807ce31013578ec5a358f435ad
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-04-01 22:21:19 +0530

    almost complete

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -5,18 +5,18 @@ const TMDB_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTQ2OWFkNWJjZGZhNWVlNDNhNmQwMzRkZTVkZDk1OCIsInN1YiI6IjY2MDZjMjc0YTZkZGNiMDE3YzQ1NjY5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jowah75c-lMBXdlDYqDvVllKADpFZgAezZXZj4ajsY0";
 
 const headers = {
-  Authorization: "bearer " + TMDB_TOKEN,
+    Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
-  try {
-    const { data } = await axios.get(BASE_URL + url, {
-      headers,
-      params,
-    });
-    return data;
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 };
