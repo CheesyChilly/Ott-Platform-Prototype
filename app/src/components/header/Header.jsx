eecd0d4431a973244c3407fc66ce0b17b420ea4f
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-04-26 21:29:28 +0530

    added login functionality

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+import { MdAccountCircle } from "react-icons/md";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./style.scss";
 
@@ -11,118 +14,139 @@ import logo from "../../assets/movix-logo.svg";
 import logo1 from "../../assets/bitstream-transparent.svg";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [location]);
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const controlNavbar = () => {
-        if (window.scrollY > 200) {
-            if (window.scrollY > lastScrollY && !mobileMenu) {
-                setShow("hide");
-            } else {
-                setShow("show");
-            }
-        } else {
-            setShow("top");
-        }
-        setLastScrollY(window.scrollY);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
+  }, [lastScrollY]);
 
-    useEffect(() => {
-        window.addEventListener("scroll", controlNavbar);
-        return () => {
-            window.removeEventListener("scroll", controlNavbar);
-        };
-    }, [lastScrollY]);
-
-    const searchQueryHandler = (event) => {
-        if (event.key === "Enter" && query.length > 0) {
-            navigate(`/search/${query}`);
-            setTimeout(() => {
-                setShowSearch(false);
-            }, 1000);
-        }
-    };
-
-    const openSearch = () => {
-        setMobileMenu(false);
-        setShowSearch(true);
-    };
-
-    const openMobileMenu = () => {
-        setMobileMenu(true);
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
         setShowSearch(false);
-    };
+      }, 1000);
+    }
+  };
 
-    const navigationHandler = (type) => {
-        if (type === "movie") {
-            navigate("/explore/movie");
-        } else {
-            navigate("/explore/tv");
-        }
-        setMobileMenu(false);
-    };
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
 
-    return (
-        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
-            <ContentWrapper>
-                <div className="logo" onClick={() => navigate("/")}>
-                    <img src={logo1} alt="" />
-                </div>
-                <ul className="menuItems">
-                    <li
-                        className="menuItem"
-                        onClick={() => navigationHandler("movie")}
-                    >
-                        Movies
-                    </li>
-                    <li
-                        className="menuItem"
-                        onClick={() => navigationHandler("tv")}
-                    >
-                        TV Shows
-                    </li>
-                    <li className="menuItem">
-                        <HiOutlineSearch onClick={openSearch} />
-                    </li>
-                </ul>
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
 
-                <div className="mobileMenuItems">
-                    <HiOutlineSearch onClick={openSearch} />
-                    {mobileMenu ? (
-                        <VscChromeClose onClick={() => setMobileMenu(false)} />
-                    ) : (
-                        <SlMenu onClick={openMobileMenu} />
-                    )}
-                </div>
-            </ContentWrapper>
-            {showSearch && (
-                <div className="searchBar">
-                    <ContentWrapper>
-                        <div className="searchInput">
-                            <input
-                                type="text"
-                                placeholder="Search for a movie or tv show...."
-                                onChange={(e) => setQuery(e.target.value)}
-                                onKeyUp={searchQueryHandler}
-                            />
-                            <VscChromeClose
-                                onClick={() => setShowSearch(false)}
-                            />
-                        </div>
-                    </ContentWrapper>
-                </div>
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else if (type === "tv") {
+      navigate("/explore/tv");
+    } else {
+      navigate("/register");
+    }
+    setMobileMenu(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo1} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">
+            <HiOutlineSearch onClick={openSearch} />
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            TV Shows
+          </li>
+          <li>
+            {isAuthenticated && (
+              <div>
+                <p className="menuItem">{user.nickname}</p>
+                {/* <p className="menuItem">{user.email}</p> */}
+              </div>
             )}
-        </header>
-    );
+          </li>
+          {isAuthenticated ? (
+            <li>
+              <li
+                className="menuItem"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }>
+                LogOut
+              </li>
+            </li>
+          ) : (
+            <li className="menuItem" onClick={() => loginWithRedirect()}>
+              Login / Register
+            </li>
+          )}
+        </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or tv show...."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import "./index.scss";
 
@@ -7,7 +8,15 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-mx6uvhaou2qfce8r.jp.auth0.com"
+    clientId="Vy84kH2yCVmE3yPkh8bstTiwztliPHZE"
+    cacheLocation="localstorage"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
+  </Auth0Provider>
 );
